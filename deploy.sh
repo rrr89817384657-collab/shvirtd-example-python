799e13dac35a3a6d0712bd792010580bde6bc206
rrr89817384657-collab <rrr89817384657@gmail.com>
2025-10-22 22:53:06 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ if [ -d "/opt/example-python" ]; then
     cd /opt/example-python
     git pull
 else
+    sudo mkdir -p /opt
+    sudo chown $USER:$USER /opt
     git clone https://github.com/rrr89817384657-collab/shvirtd-example-python.git /opt/example-python
     cd /opt/example-python
 fi
